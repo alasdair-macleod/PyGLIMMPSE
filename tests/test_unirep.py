272780edd1d451dc45b2b228509cafa28fae32e8
Qian <Andeeli@users.noreply.github.com>
2018-05-10 12:57:20 -0400

    VER3-352 fix broken tests

--- tests/test_unirep.py
+++ tests/test_unirep.py
@@ -9,7 +9,7 @@ class TestUnirep(TestCase):
     def test_hfexeps(self):
         """ should return expected value """
         expected = 0.2901679
-        actual = unirep.hyuhn_feldt_muller_edwards_simpson_taylor_2004(sigma_star=np.matrix([[1, 2, 3], [3, 4, 5], [4, 5, 6]]),
+        actual = unirep.hyuhn_feldt_muller_edwards_simpson_taylor_2007(sigma_star=np.matrix([[1, 2, 3], [3, 4, 5], [4, 5, 6]]),
                                 rank_U=3,
                                 total_N=20,
                                 rank_X=5)
@@ -18,7 +18,7 @@ class TestUnirep(TestCase):
     def test_cmexeps(self):
         """ should return expected value """
         expected = 0.2757015
-        actual = unirep.chi_muller_muller_edwards_simpson_taylor_2004(sigma_star=np.matrix([[1, 2, 3], [3, 4, 5], [4, 5, 6]]),
+        actual = unirep.chi_muller_muller_edwards_simpson_taylor_2007(sigma_star=np.matrix([[1, 2, 3], [3, 4, 5], [4, 5, 6]]),
                                 rank_U=3,
                                 total_N=20,
                                 rank_X=5)
@@ -27,7 +27,7 @@ class TestUnirep(TestCase):
     def test_ggexeps(self):
         """ should return expected value """
         expected = 0.2975125
-        actual = unirep.geisser_greenhouse_muller_edwards_simpson_taylor_2004(sigma_star=np.matrix([[1, 2, 3], [3, 4, 5], [4, 5, 6]]),
+        actual = unirep.geisser_greenhouse_muller_edwards_simpson_taylor_2007(sigma_star=np.matrix([[1, 2, 3], [3, 4, 5], [4, 5, 6]]),
                                 rank_U=3,
                                 total_N=20,
                                 rank_X=5)
