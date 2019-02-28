44e96783dc6d8d2ad315d6c39d1d3443cec9d86d
Qian <Andeeli@users.noreply.github.com>
2019-02-28 15:33:36 -0500

    VER3-273 fixsamplesize calculation. needs a refactor. we need to check that we are using scale factors properly. we may be vulnerable to use of theta - theta 0

--- pyglimmpse/samplesize.py
+++ pyglimmpse/samplesize.py
@@ -13,15 +13,12 @@ def samplesize(test,
                rank_C,
                rank_U,
                alpha,
-               sigmaScale,
-               sigma,
-               betaScale,
-               beta,
+               sigma_star,
                targetPower,
                rank_X,
-               eval_HINVE=None,
-               error_sum_square=None,
-               hypothesis_sum_square=None,
+               m,
+               t,
+               groups,
                starting_sample_size=Constants.STARTING_SAMPLE_SIZE.value,
                optional_args=None):
     """
@@ -30,67 +27,77 @@ def samplesize(test,
     :param rank_C:
     :param rank_U:
     :param alpha:
-    :param sigmaScale:
-    :param sigma:
-    :param betaScale:
-    :param beta:
+    :param sigma_star:
     :param targetPower:
     :param rank_X:
-    :param eval_HINVE:
-    :param error_sum_square:
-    :param hypothesis_sum_square:
     :param starting_sample_size:
     :param optional_args:
     :return:
     """
 
-    # scale beta and sigma matrices
-    # TODO: how to include this in the finished product?
-    scaled_beta = beta * betaScale
-    scaled_sigma = sigma * sigmaScale
-
     # calculate max valid per group N
     max_n = min(sys.maxsize/rank_X, Constants.MAX_SAMPLE_SIZE.value)
 
-    # calculate the noncentrality distribution
+    # declare error_sum_square
+    error_sum_square = None
+    # declare hypothesis_sum_square
+    hypothesis_sum_square = None
 
-    # find a samplesize which produces power greater than or equal to the desired power
-    upper_bound = starting_sample_size
+    starting_sample_size = 3
+
+    test_smallest_group_size = starting_sample_size
     upper_power = Power()
-    while (upper_power.power == np.NaN or upper_power.power <= targetPower)\
-           and upper_bound < max_n:
-        upper_bound += upper_bound
+
+    # TODO: consider the condition upper bound less than max_n
+    upper_bound = starting_sample_size
+    while (upper_power.power == np.NaN or upper_power.power <= targetPower) and upper_bound < max_n:
+        #upper_bound += upper_bound
+
+        # find a samplesize which produces power greater than or equal to the desired power
+        upper_bound = sum([test_smallest_group_size * g for g in groups])
 
         if upper_bound >= max_n:
             upper_bound = max_n
 
         total_N = upper_bound
 
+        #correct
+        error_sum_square = (total_N - rank_X) * sigma_star
+        #incorrect
+        repeated_rows_in_design_matrix = total_N/sum([g for g in groups])
+        hypothesis_sum_square = repeated_rows_in_design_matrix * np.transpose(t) * np.linalg.inv(m) * t
+
         # call power for this sample size
         if len(inspect.signature(test).parameters) == 8:
             upper_power = test(rank_C=rank_C,
                                rank_U=rank_U,
                                rank_X=rank_X,
-                               total_N=upper_bound,
+                               total_N=total_N,
                                alpha=alpha,
                                error_sum_square=error_sum_square,
                                hypothesis_sum_square=hypothesis_sum_square)
         elif len(inspect.signature(test).parameters) == 9:
             upper_power = test(rank_C=rank_C,
                                rank_U=rank_U,
-                               total_N=upper_bound,
+                               total_N=total_N,
                                rank_X=rank_X,
                                error_sum_square=error_sum_square,
                                hypo_sum_square=hypothesis_sum_square,
-                               sigma_star=sigma,
+                               sigma_star=sigma_star,
                                alpha=alpha,
                                optional_args=optional_args)
         if type(upper_power.power) is str:
             raise ValueError('Upper power is not calculable. Check that your design is realisable.'
                              ' Usually the easies way to do this is to increase sample size')
+        test_smallest_group_size += test_smallest_group_size
 
     # note we are using floor division
-    lower_bound = upper_bound//2 + 1
+    #lower_bound = upper_bound//2 + 1
+    test_smallest_group_size = test_smallest_group_size / 2
+    lower_bound = sum([(test_smallest_group_size//2) * g for g in groups])
+    error_sum_square = (lower_bound - rank_X) * sigma_star
+    repeated_rows_in_design_matrix = lower_bound / sum([g for g in groups])
+    hypothesis_sum_square = repeated_rows_in_design_matrix * np.transpose(t) * np.linalg.inv(m) * t
     lower_power = Power()
     if len(inspect.signature(test).parameters) == 8:
             lower_power = test(rank_C=rank_C,
@@ -107,7 +114,7 @@ def samplesize(test,
                            rank_X=rank_X,
                            error_sum_square=error_sum_square,
                            hypo_sum_square=hypothesis_sum_square,
-                           sigma_star=sigma,
+                           sigma_star=sigma_star,
                            alpha=alpha,
                            optional_args=optional_args)
 
@@ -129,22 +136,25 @@ def samplesize(test,
             f = lambda n: subtrtact_target_power(test( rank_C=rank_C,
                                                        rank_U=rank_U,
                                                        rank_X=rank_X,
-                                                       total_N=n,
+                                                       total_N=sum([n * g for g in groups]),
                                                        alpha=alpha,
                                                        error_sum_square=error_sum_square,
                                                        hypothesis_sum_square=hypothesis_sum_square), targetPower)
         elif len(inspect.signature(test).parameters) == 9:
-            f = lambda n: subtrtact_target_power(test( rank_C=rank_C,
-                                                       rank_U=rank_U,
-                                                       total_N=n,
-                                                       rank_X=rank_X,
-                                                       error_sum_square=error_sum_square,
-                                                       hypo_sum_square=hypothesis_sum_square,
-                                                       sigma_star=sigma,
-                                                       alpha=alpha,
-                                                       optional_args=optional_args), targetPower)
-        total_N = optimize.bisect(f, lower_bound, upper_bound)
-        total_N = math.ceil(total_N)
+            f = lambda n: subtrtact_target_power(test(rank_C=rank_C,
+                                                      rank_U=rank_U,
+                                                      total_N=sum([n * g for g in groups]),
+                                                      rank_X=rank_X,
+                                                      error_sum_square=error_sum_square,
+                                                      hypo_sum_square=hypothesis_sum_square,
+                                                      sigma_star=sigma_star,
+                                                      alpha=alpha,
+                                                      optional_args=optional_args), targetPower)
+        total_N = optimize.bisect(f, test_smallest_group_size//2, test_smallest_group_size)
+        total_N = sum( [ math.ceil(total_N) * g for g in groups])
+        error_sum_square = (total_N - rank_X) * sigma_star
+        repeated_rows_in_design_matrix = total_N / sum([g for g in groups])
+        hypothesis_sum_square = repeated_rows_in_design_matrix * np.transpose(t) * np.linalg.inv(m) * t
 
 
         if len(inspect.signature(test).parameters) == 8:
@@ -162,10 +172,10 @@ def samplesize(test,
                         rank_X=rank_X,
                         error_sum_square=error_sum_square,
                         hypo_sum_square=hypothesis_sum_square,
-                        sigma_star=sigma,
+                        sigma_star=sigma_star,
                         alpha=alpha,
                         optional_args=optional_args)
 
     if power.power < targetPower:
-        raise ValueError('Samplesize cannot be calculated. Pleas check your design.')
+        raise ValueError('Samplesize cannot be calculated. Please check your design.')
     return total_N, power.power
