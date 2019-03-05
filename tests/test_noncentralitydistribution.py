8515e13d16a65361aaed1fe8c7574e5ba408210e
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-03-05 13:40:37 -0500

    VER3-470 fix noncentrality dist test

--- tests/test_noncentralitydistribution.py
+++ tests/test_noncentralitydistribution.py
@@ -25,58 +25,35 @@ class TestUnirep(TestCase):
         # beta=np.matrix([[1.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.9, 0.0]]),
         # sigmaError=np.matrix([[0.19, 0.0],[0.0, 1.0]]) - (np.matrix([[0.9], [0.0]]) * np.matrix([[0.9], [0]]).T),
 
+        print("H0", a.getH0())
+        self.assertAlmostEqual(0.0, a.getH0(), 7)
+        print("H1", a.getH1())
+        self.assertAlmostEqual(63.33333333333333, a.getH1(), 7)
         print("Answer: ", a.cdf(1))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(2))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(3))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(4))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(5))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(6))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(7))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(8))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(9))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(10))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(11))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(12))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(13))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
-
         print("Answer: ", a.cdf(14))
-        print("H0", a.getH0())
-        print("H1", a.getH1())
+        self.assertAlmostEqual(0.0000002106, a.cdf(1),  4)
+        self.assertAlmostEqual(0.0000003420, a.cdf(2),  4)
+        self.assertAlmostEqual(0.0000014004, a.cdf(3),  4)
+        self.assertAlmostEqual(0.0000057429, a.cdf(4),  4)
+        self.assertAlmostEqual(0.0000182151, a.cdf(5),  4)
+        self.assertAlmostEqual(0.0000470019, a.cdf(6),  4)
+        self.assertAlmostEqual(0.0001043387, a.cdf(7),  4)
+        self.assertAlmostEqual(0.0002070572, a.cdf(8),  4)
+        self.assertAlmostEqual(0.0003769546, a.cdf(9),  4)
+        self.assertAlmostEqual(0.0006409878, a.cdf(10), 4)
+        self.assertAlmostEqual(0.0010313008, a.cdf(11), 4)
+        self.assertAlmostEqual(0.0015850977, a.cdf(12), 4)
+        self.assertAlmostEqual(0.0023443774, a.cdf(13), 4)
+        self.assertAlmostEqual(0.0033555463, a.cdf(14), 4)
