b0ddcebab1c979d2a74a58ca570a8f60e0c41a01
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-03-13 15:09:49 -0400

    VER3-473 fix inverseCdf test

--- tests/test_noncentralitydistribution.py
+++ tests/test_noncentralitydistribution.py
@@ -6,7 +6,7 @@ from pyglimmpse.WeightedSumOfNoncentralChiSquaresDistribution import *
 from pyglimmpse.probf import probf
 
 
-class TestUnirep(TestCase):
+class TestNoncentralityDist(TestCase):
 
     def test_noncentralitydistribution(self):
         a = NonCentralityDistribution(
@@ -53,6 +53,48 @@ class TestUnirep(TestCase):
         self.assertAlmostEqual(0.0015850977, a.cdf(12), 4)
         self.assertAlmostEqual(0.0023443774, a.cdf(13), 4)
         self.assertAlmostEqual(0.0033555463, a.cdf(14), 4)
+
+    def test_noncentralitydistribution_2(self):
+
+        Cf = np.matrix([[1.0, -1.0, 0.0], [1.0, 0.0, -1.0]])
+        Cg = np.matrix([[1.0], [1.0]])
+        C = np.concatenate((Cf, Cg), axis=1)
+        B = np.matrix([[1.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.9, 0.0]])
+        U = np.matrix([[1.0, 0.0], [0.0, 1.0]])
+        Theta = C*B*U
+        Theta0=np.matrix([[0.0, 0.0],[0.0, 0.0]])
+        thetaDiff = Theta - Theta0
+        FEssence = np.matrix([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+        SigmaError= np.matrix([[0.19, 0.0], [0.0, 1.0]])
+        sigmaStar = np.matrix([[0.19,0.0],[0.0,1.0]])
+        stdevG = 1.0
+        perGroupN = 5
+
+        a = NonCentralityDistribution(
+            test=Constants.HLT.value,
+            FEssence=FEssence,
+            FtFinverse=FEssence,
+            perGroupN=perGroupN,
+            CFixed=Cf,
+            CRand=Cg,
+            thetaDiff=thetaDiff,
+            stddevG=stdevG,
+            sigmaStar= sigmaStar,
+            exact=False)
+        print("H0", a.getH0())
+        print("H1", a.getH1())
+        print("Answer: ", a.cdf(1))
+        print("Answer: ", a.inverseCDF(0.5))
+        self.assertAlmostEqual(30.07007667770580, a.inverseCDF(0.1))
+        self.assertAlmostEqual(36.147160249831344, a.inverseCDF(0.2))
+        self.assertAlmostEqual(40.66756344933491, a.inverseCDF(0.3))
+        self.assertAlmostEqual(44.52591765312671, a.inverseCDF(0.4))
+        self.assertAlmostEqual(48.04932724557611, a.inverseCDF(0.5))
+        self.assertAlmostEqual(51.41016454949209, a.inverseCDF(0.6))
+        self.assertAlmostEqual(54.72284790812153, a.inverseCDF(0.7))
+        self.assertAlmostEqual(58.06316056229026, a.inverseCDF(0.8))
+        self.assertAlmostEqual(61.361079235392985, a.inverseCDF(0.9))
+        self.assertAlmostEqual(63.33333333333333, a.inverseCDF(1.0))
 
     def test_probf(self):
         print(probf(0.05, 0.5, 0.5, 10.0))
