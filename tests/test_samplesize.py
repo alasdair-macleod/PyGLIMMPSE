VER3-502 fix samplesize calculation

tests/test_samplesize.py
@@ -32,8 +32,8 @@ class TestSamplesize(TestCase):
                                             delta_es=delta,
                                             relative_group_sizes=groups,
                                             starting_smallest_group_size=10)
-        self.assertEqual(50, size)
-        self.assertEqual(round(power.power, 8), round(0.9, 8))
+        self.assertEqual(49, size)
+        self.assertEqual(round(power.power, 8), round(0.90101951, 8))
 
     def test_samplesize_hlt_multi_group(self):
         m=np.matrix([[1.16666667, 0.16666667], [0.16666667, 0.66666667]])
@@ -54,8 +54,8 @@ class TestSamplesize(TestCase):
                                             delta_es=delta,
                                             relative_group_sizes=groups)
 
-        self.assertEqual(round(0.9086544301747213, 8), round(power.power, 8))
-        self.assertEqual(369, size)
+        self.assertEqual(round(0.90459334, 8), round(power.power, 8))
+        self.assertEqual(360, size)
 
 
     # def test_samplesize_uncorrected(self):

tests/test_unirep.py
@@ -8,6 +8,32 @@ from pyglimmpse.constants import Constants
 from pyglimmpse.unirep import _geisser_greenhouse_muller_edwards_simpson_taylor_2007, _calc_epsilon, OptionalArgs
 
 
+class ConfidenceInterval(object):
+    """
+    Class holding required information to calculate confidence intervals.
+    """
+
+    def __init__(self,
+                 beta_known: bool = True,
+                 lower_tail: float = 0.05,
+                 upper_tail: float = 0.0,
+                 rank_est: int = 1,
+                 n_est: int = 1):
+        """
+        Class describing a Confidence Interval
+        :param beta_known:
+        :param lower_tail:
+        :param upper_tail:
+        :param rank_est:
+        :param n_est:
+        :param kwargs:
+        """
+        self.beta_known = beta_known
+        self.lower_tail = lower_tail
+        self.upper_tail = upper_tail
+        self.rank_est = rank_est
+        self.n_est = n_est
+
 class TestUnirep(TestCase):
 
     def test_geisser_greenhouse_muller_barton_1989(self):
@@ -97,12 +123,14 @@ class TestUnirep(TestCase):
         rank_X = 1
         total_N = 20
 
-        n_est = 10
-        rank_est = 1
+        confidence_interval = ConfidenceInterval(beta_known=False,
+                                                 lower_tail=0.01,
+                                                 upper_tail=0.01,
+                                                 n_est=10,
+                                                 rank_est=1)
+
         tolerance = 0.000000000000001
 
-        alpha_cl = 0.01
-        alpha_cu = 0.01
 
         hypo_sum_square = np.matrix([[0.3125, 0.625, -0.625, 0.3125],
                                      [0.625, 1.25, -1.25, 0.625],
@@ -128,10 +156,7 @@ class TestUnirep(TestCase):
                                                       approximation=approximation,
                                                       epsilon_estimator=Constants.EPSILON_MULLER2004,
                                                       unirepmethod=Constants.SIGMA_KNOWN,
-                                                      n_est=n_est,
-                                                      rank_est=rank_est,
-                                                      alpha_cl=alpha_cl,
-                                                      alpha_cu=alpha_cu,
+                                                      confidence_interval=confidence_interval,
                                                       n_ip=1,
                                                       rank_ip=1,
                                                       tolerance=tolerance)
@@ -147,15 +172,16 @@ class TestUnirep(TestCase):
         rank_X = 1
         total_N = 20
 
-        alpha_cl = 0.01
-        alpha_cu = 0.01
+        confidence_interval = ConfidenceInterval(beta_known=False,
+                                                 lower_tail=0.01,
+                                                 upper_tail=0.01,
+                                                 n_est=10,
+                                                 rank_est=1)
+
         tolerance = 0.000000000000001
 
         approximation = Constants.GG
         unirepmethod = Constants.UCDF_MULLER2004_APPROXIMATION
-
-        n_est=10
-        rank_est=1
 
         hypo_sum_square = np.matrix([[0.3125, 0.625, -0.625, 0.3125],
                                      [0.625, 1.25, -1.25, 0.625],
@@ -190,10 +216,7 @@ class TestUnirep(TestCase):
                                                       approximation=approximation,
                                                       epsilon_estimator=Constants.EPSILON_MULLER2004.value,
                                                       unirepmethod=Constants.SIGMA_KNOWN.value,
-                                                      n_est=n_est,
-                                                      rank_est=rank_est,
-                                                      alpha_cl=alpha_cl,
-                                                      alpha_cu=alpha_cu,
+                                                      confidence_interval=confidence_interval,
                                                       n_ip=1,
                                                       rank_ip=1,
                                                       tolerance=tolerance)