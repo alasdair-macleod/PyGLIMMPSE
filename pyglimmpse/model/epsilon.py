fix samplesize calculation bugs and huyhn-feldt degrees of freedom errors

pyglimmpse/model/epsilon.py
@@ -51,6 +51,6 @@ class Epsilon:
     def esigEvals(self):
         # get the eigenvalues of self.esig using a singular value decomposition
         # seval is an array of dimension 1 x b
-        seval = np.linalg.svd(self.esig, full_matrices=False, compute_uv=False, hermitian=True).T
+        seval = np.matrix(np.linalg.svd(self.esig, full_matrices=False, compute_uv=False, hermitian=True)).T
         esigEvals = np.sum(seval * seval.T)
         return esigEvals

pyglimmpse/samplesize.py
@@ -7,6 +7,7 @@ import sys
 from pyglimmpse.constants import Constants
 from pyglimmpse.model.power import Power, subtrtact_target_power
 from scipy import optimize
+from pyglimmpse.exceptions.glimmpse_exception import GlimmpseValidationException
 
 
 def samplesize(test,
@@ -57,16 +58,19 @@ def samplesize(test,
             upper_bound_smallest_group_size = upper_bound_total_N/max_n
 
         # call power for this sample size
-        upper_power = test(rank_C=rank_C,
-                           rank_X=rank_X,
-                           relative_group_sizes=relative_group_sizes,
-                           rep_N=upper_bound_smallest_group_size,
-                           alpha=alpha,
-                           sigma_star=sigma_star,
-                           delta_es=delta_es)
-        if type(upper_power.power) is str:
-            raise ValueError('Upper power is not calculable. Check that your design is realisable.'
-                             ' Usually the easies way to do this is to increase sample size')
+        try:
+            upper_power = test(rank_C=rank_C,
+                               rank_X=rank_X,
+                               relative_group_sizes=relative_group_sizes,
+                               rep_N=upper_bound_smallest_group_size,
+                               alpha=alpha,
+                               sigma_star=sigma_star,
+                               delta_es=delta_es)
+            if type(upper_power.power) is str:
+                raise ValueError('Upper power is not calculable. Check that your design is realisable.'
+                                 ' Usually the easies way to do this is to increase sample size')
+        except GlimmpseValidationException as e:
+            upper_power.power == np.nan
         upper_bound_smallest_group_size += upper_bound_smallest_group_size
         if not np.isnan(upper_power.power) and not smallest_design_found:
             lowest_realizeable_power = upper_power

pyglimmpse/unirep.py
@@ -1,3 +1,4 @@
+import math
 import warnings
 import inspect
 import numpy as np
@@ -755,6 +756,9 @@ def _hyuhn_feldt_muller_edwards_simpson_taylor_2007(sigma_star: np.matrix, rank_
     epsilon = _calc_epsilon(sigma_star, rank_U)
     # Computation of EXP(T1) and EXP(T2)
     nu = total_N - rank_X
+    # for valid error degrees of freedom, nu must be strictly greater than 4
+    if nu < 4:
+        return np.nan
     expt1 = 2 * nu * epsilon.slam2 + nu ** 2 * epsilon.slam1
     expt2 = nu * (nu + 1) * epsilon.slam2 + nu * epsilon.esigEvals()
     num01 = (1 / rank_U) * ((nu + 1) * expt1 - 2 * expt2)
@@ -1131,7 +1135,7 @@ def _calc_undf1_undf2(unirep_method, exeps, nue, rank_C, rank_U):
         # raise GlimmpseValidationException('Power is missing, because Uncorrected, Geisser-Greenhouse and Box tests are'
         #               'poorly behaved (super low power and test size) when B > N-R, i.e., HDLSS.')
     if np.isnan(exeps) or nue <= 0:
-        raise GlimmpseValidationException("exeps is NaN or total_N  <= rank_X")
+        raise GlimmpseValidationException(Constants.ERR_ERROR_DEG_FREEDOM.value)
     undf1 = rank_C * rank_U
     undf2 = rank_U * nue
     return undf1, undf2